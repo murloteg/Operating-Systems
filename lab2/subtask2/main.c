127446d1982e41ad231597670cf43b7c67e2f921
Kirill Bolotov <k.y.bolotov@gmail.com>
2023-04-02 18:46:39 +0700

    Fixed some problems with this code

--- lab2/subtask2/main.c
+++ lab2/subtask2/main.c
@@ -4,12 +4,11 @@
 #include <sys/syscall.h>
 #include <errno.h>
 
-enum ErrorCodes {
-    SOMETHING_WENT_WRONG = -1
-};
-
-ssize_t call_write(int fileDescriptor, const void* buffer, size_t count) {
+int main() {
+    char* message = "Hello, world!\n";
+    unsigned long countOfBytes = 14;
     int syscallStatus = 0;
+
     asm volatile (
             "movl %[syscallNumber], %%eax\n"    /* "zero"-operand */
             "movl %[fileDescriptor], %%edi\n"   /* "first"-operand */
@@ -17,25 +16,15 @@ ssize_t call_write(int fileDescriptor, const void* buffer, size_t count) {
             "movq %[count], %%rdx\n"            /* "third"-operand */
             "syscall\n"
             "movl %%eax, %[syscallStatus]"
-            : [syscallStatus] "=r" (syscallStatus)  /* output operands */
-            : [syscallNumber] "i" (SYS_write), [fileDescriptor] "r" (fileDescriptor),    /* input operands */
-                                        [buffer] "r" (buffer), [count] "r" (count)
+            : [syscallStatus] "=r" (syscallStatus)      /* output operands */
+            : [syscallNumber] "i" (SYS_write), [fileDescriptor] "i" (STDOUT_FILENO),
+                        [buffer] "r" (message), [count] "r" (countOfBytes)     /* input operands */
             : "eax"     /* clobbered-list */
     );
 
     if (syscallStatus < 0) {
         errno = -syscallStatus;
-        return SOMETHING_WENT_WRONG;
-    }
-}
-
-int main() {
-    char* message = "Hello, world!\n";
-    size_t count = 14;
-    ssize_t isSuccessfulCalling = call_write(STDOUT_FILENO, message, count);
-
-    if (isSuccessfulCalling == SOMETHING_WENT_WRONG) {
-        perror("Write error");
+        perror("Error!");
         return EXIT_FAILURE;
     }
     return EXIT_SUCCESS;
