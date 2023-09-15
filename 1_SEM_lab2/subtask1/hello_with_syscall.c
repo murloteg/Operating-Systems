e6eb1d1fdb01ae804ce0826b3fa83793067ef801
Kirill Bolotov <k.y.bolotov@gmail.com>
2023-09-15 23:42:51 +0700

    Исправлен code style

--- 1_SEM_lab2/subtask1/hello_with_syscall.c
+++ 1_SEM_lab2/subtask1/hello_with_syscall.c
@@ -2,19 +2,23 @@
 #include <unistd.h>
 #include <sys/syscall.h>
 
-enum FileDescriptorsConsts {
+enum util_consts {
+    STRING_LENGTH = 14
+};
+
+enum file_descriptors {
     STDOUT = 1
 };
 
-enum ErrorCodes {
+enum statuses {
     SOMETHING_WENT_WRONG = -1
 };
 
 int call_write() {
-    return syscall(SYS_write, STDOUT, "Hello, World!\n", 14);
+    return syscall(SYS_write, STDOUT, "Hello, World!\n", STRING_LENGTH);
 }
 
-int main() {
+int main(int argc, char** argv) {
     int status = call_write();
     if (status == SOMETHING_WENT_WRONG) {
         return EXIT_FAILURE;
--- 1_SEM_lab2/subtask2/main.c
+++ 1_SEM_lab2/subtask2/main.c
@@ -4,32 +4,32 @@
 #include <sys/syscall.h>
 #include <errno.h>
 
-enum ErrorCodes {
+enum statuses {
     SOMETHING_WENT_WRONG = -1
 };
 
-ssize_t call_write(int fileDescriptor, const void* buffer, size_t countOfBytes) {
-    int syscallStatus = 0;
+ssize_t call_write(int file_descriptor, const void* buffer, size_t count_of_bytes) {
+    int syscall_status = 0;
     asm volatile (
             "syscall"
-            : "=a" (syscallStatus)
-            : "a" (SYS_write), "D" (fileDescriptor), "S" (buffer), "d" (countOfBytes)
+            : "=a" (syscall_status)
+            : "a" (SYS_write), "D" (file_descriptor), "S" (buffer), "d" (count_of_bytes)
             );
 
-    if (syscallStatus < 0) {
-        errno = -syscallStatus;
+    if (syscall_status < 0) {
+        errno = -syscall_status;
         return SOMETHING_WENT_WRONG;
     }
-    return syscallStatus;
+    return syscall_status;
 }
 
-int main() {
-    size_t countOfBytes = 14;
+int main(int argc, char** argv) {
+    size_t count_of_bytes = 14;
     char* message = "Hello, world!\n";
 
-    ssize_t syscallWriteStatus = call_write(STDOUT_FILENO, message, countOfBytes);
-    if (syscallWriteStatus == SOMETHING_WENT_WRONG) {
-        perror("Error");
+    ssize_t syscall_write_status = call_write(STDOUT_FILENO, message, count_of_bytes);
+    if (syscall_write_status == SOMETHING_WENT_WRONG) {
+        perror("Error during call_write()");
         return EXIT_FAILURE;
     }
     return EXIT_SUCCESS;
